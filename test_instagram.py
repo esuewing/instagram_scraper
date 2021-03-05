db0eadfc2c6093df1527e832fb8942644ad28c2c
tony <tony@alethio.io>
2021-03-05 09:56:52 +0900

    CHANGE config

--- test_instagram.py
+++ test_instagram.py
@@ -21,14 +21,17 @@ class InstagramTests():
         # This is a max id of the last item in response_first_page.json.
         #self.max_id = "1369793132326237681_50955533"
 
+        save_dir = '/media/alethio/esue_hdd/1_image_raw'
+        #self.test_dir = './temp'
 
         #tags = ['내가웃는게태교다']
         #tags = ['신생아가디건']
-        tags = ['생후56일']
         #tags = ['기내에서맥주먹는느낌내봤지모야']
+        tags = ['생후56일']
+        tag_dir = '56d'
 
-        #self.test_dir = './temp'
-        self.test_dir = '/media/alethio/esue_hdd/1_image_raw/56d'
+
+        self.test_dir = os.path.join(save_dir, tag_dir)
 
 
         args = {
